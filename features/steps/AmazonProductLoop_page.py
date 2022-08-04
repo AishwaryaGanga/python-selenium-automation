d8f93eee932954161be261fdd987bc8f78010dff
AishwaryaGanga <aishwaryaganga.s@gmail.com>
2022-08-04 15:01:04 -0700

    HomeWork

--- features/steps/AmazonProductLoop_page.py
+++ features/steps/AmazonProductLoop_page.py
@@ -3,6 +3,7 @@ from behave import given, when, then
 
 CHECK_PRODUCT_NAME = (By.CSS_SELECTOR,'.a-size-mini.a-spacing-none.a-color-base')
 CHECK_PRODUCT_IMAGE = (By.CSS_SELECTOR, '.a-section.aok-relative.s-image-square-aspect')
+#CHECK_PRODUCT_IMAGE = (By.CSS_SELECTOR, '.s-image[data-image-latency="s-product-image"]')
 WHOLE_BLOCK = (By.CSS_SELECTOR, '.a-section.a-spacing-base')
 
 
@@ -10,22 +11,26 @@ WHOLE_BLOCK = (By.CSS_SELECTOR, '.a-section.a-spacing-base')
 def open_search_result(context):
     context.driver.get('https://www.amazon.com/s?k=coffee&crid=3MSJWFMI487WB&sprefix=coffee%2Caps%2C140&ref=nb_sb_noss_1')
 
-@then('Verify has a {expected_name}.')
-def product_name(context,expected_name):
-    actual_name = context.driver.find_elements(*CHECK_PRODUCT_NAME).text
-    assert expected_name == actual_name, f'expected{expected_name} but got {actual_name}'
+@then('Verify has a product name.')
+def product_name(context):
+    name = context.driver.find_elements(*CHECK_PRODUCT_NAME)
+    print(len(name))
 
-@then('Verify has a {expected_img}.')
-def product_img(context, expected_img):
-    actual_img = context.driver.find_elements(*CHECK_PRODUCT_IMAGE)
-    assert expected_img == actual_img, f'expected{expected_img} but got {actual_img}'
+@then('Verify has a image of the products.')
+def product_img(context):
+    image = context.driver.find_elements(*CHECK_PRODUCT_IMAGE)
+    print(len(image))
 
-@then('Check block has {expected_result}')
-def check_block(context, expected_result):
-    for prod in check_block:
-        product = context.driver.find_elements(*WHOLE_BLOCK)
-        title = prod.find_elemet(*CHECK_PRODUCT_NAME).text
-        img = prod.find_elemet(*CHECK_PRODUCT_IMAGE)
+@then('Check block has name and image.')
+def check_block(context):
+    products = context.driver.find_elements(*WHOLE_BLOCK)
+
+    for prod in products:
+        title = prod.find_element(*CHECK_PRODUCT_NAME).text
+        print('\n\n',title)
+        assert title, 'Error!Its blank'
+        prod.find_element(*CHECK_PRODUCT_IMAGE)
+        print('Img Element:',prod.find_element(*CHECK_PRODUCT_IMAGE))
 
 
 
--- features/tests/AmazonProduct_Loop.feature
+++ features/tests/AmazonProduct_Loop.feature
@@ -4,5 +4,5 @@ Feature: Test a page with product name and image
   Scenario: Check product image and name.
     Given Open the search result page.
     Then Verify has a product name.
-    Then Verify has a product image.
+    Then Verify has a image of the products.
     Then Check block has name and image.
